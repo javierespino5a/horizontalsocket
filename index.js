96314b44f0214dd8441192b469d10918e919f48d
Javier Espinosa <javier.espinosa@verticalknits.com>
2022-01-05 11:09:02 -0600

    cambios

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ io.on('connection', (socket) =>{
       });
 
       socket.on('chat message', (msg,room,usuario) => {
+        console.log("llgo")
         conexion.connect(parametros, function(err) {
           if (err) throw err;
           conexion.exec('select count(config."WtmCode") as total from "VERTICAL_PRODUCTIVO"."OWTM" config inner join "VERTICAL_PRODUCTIVO"."WTM1" det_config on det_config."WtmCode" = config."WtmCode" inner join "VERTICAL_PRODUCTIVO"."OUSR" usuario on usuario."USERID" = det_config."UserID"'
